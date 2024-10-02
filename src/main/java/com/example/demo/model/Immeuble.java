f063768d7164280f00ee029b7119e79da76e9c2c
Steven1805 <hei.steven.2@gmail.com>
2024-10-02 16:58:43 +0300

    feat: add main function in Immuble.java for testing the app functions

--- src/main/java/com/example/demo/model/Immeuble.java
+++ src/main/java/com/example/demo/model/Immeuble.java
@@ -29,4 +29,19 @@ class Immeuble extends Endroit {
     public int compterAppartements() {
         return appartements.size();
     }
+
+    public static void main(String[] args) {
+        Quartier quartier = new Quartier(1, "Ivandry");
+        Immeuble immeuble = new Immeuble(1, "Jade", 5);
+        quartier.ajouterImmeuble(immeuble);
+
+        Appartement appartement = new Appartement(1, "Appart à louer", "3 pièces", 500.0, Statut.LIBRE, immeuble);
+        immeuble.ajouterAppartement(appartement);
+
+        boolean estDansLeQuartier = quartier.contientAppartement(appartement);
+        System.out.println("L'appartement est dans le quartier : " + estDansLeQuartier);
+
+        int nombreAppartements = quartier.compterAppartements();
+        System.out.println("Nombre d'appartements dans le quartier : " + nombreAppartements);
+    }
 }
