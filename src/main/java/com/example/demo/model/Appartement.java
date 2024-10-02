81aac3cd68d8afa392b363820f3b6507c573b844
Steven1805 <hei.steven.2@gmail.com>
2024-10-02 16:14:11 +0300

    initial commit

---
+++ src/main/java/com/example/demo/model/Appartement.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class Appartement {
+    private int idAppartement;
+    private String titre;
+    private String description;
+    private double loyer;
+    private String statut;
+    private Endroit endroit;
+}
---
+++ src/main/java/com/example/demo/model/Endroit.java
@@ -0,0 +1,7 @@
+package com.example.demo.model;
+
+public class Endroit {
+    private int idEndroit;
+    private int nom;
+    private String type;
+}
